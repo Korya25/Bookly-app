618c7503e5f53fd65e61f14f81d5a9f69b468f0f
Korya25 <enthrj3@gmail.com>
2025-03-18 18:57:56 +0200

    - add dartz packege - add failure class - refactor home_repo add Either class

--- lib/features/books/domain/repos/home_repo.dart
+++ lib/features/books/domain/repos/home_repo.dart
@@ -1,7 +1,9 @@
+import 'package:book_app/core/errors/failure.dart';
 import 'package:book_app/features/books/domain/entities/book_entity.dart';
+import 'package:dartz/dartz.dart';
 
 abstract class HomeRepo {
-  Future<List<BookEntity>> getFeaturedBooks();
-  Future<List<BookEntity>> getNewestBooks();
-  Future<List<BookEntity>> getPopularBooks();
+  Future<Either<Failure, List<BookEntity>>> getFeaturedBooks();
+  Future<Either<Failure, List<BookEntity>>> getNewestBooks();
+  Future<Either<Failure, List<BookEntity>>> getSimilarBooks();
 }
