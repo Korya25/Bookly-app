a22c26492138f7e910da7fe4d23c6507379e0946
Korya25 <enthrj3@gmail.com>
2025-03-18 19:37:51 +0200

    fetch popular and newest books use case

---
+++ lib/features/books/domain/use_case/fetch_popular_books_use_case.dart
@@ -0,0 +1,16 @@
+import 'package:book_app/core/errors/failure.dart';
+import 'package:book_app/core/use_case/use_cases.dart';
+import 'package:book_app/features/books/domain/entities/book_entity.dart';
+import 'package:book_app/features/books/domain/repos/home_repo.dart';
+import 'package:dartz/dartz.dart';
+
+class FetchPopularBooksUseCase extends UseCase<List<BookEntity>, NoParm> {
+  final HomeRepo homeRepo;
+
+  FetchPopularBooksUseCase({required this.homeRepo});
+
+  @override
+  Future<Either<Failure, List<BookEntity>>> call([NoParm? param]) async {
+    return await homeRepo.getPopularBooks();
+  }
+}
