253a1817b288f29876a92291fb19709cbc9de1dc
Korya25 <enthrj3@gmail.com>
2025-03-20 02:19:52 +0200

    register adaptor and open featured box

--- lib/core/constant/app_constant.dart
+++ lib/core/constant/app_constant.dart
@@ -3,4 +3,5 @@ import 'package:book_app/core/constant/app_dimens.dart';
 class AppConstants {
   static const int carouselSliderItemsCount = 4;
   static const double sliderCardHeight = AppDimens.size350;
+  static const kFeaturedBox = 'featured_books';
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
+import 'package:book_app/core/constant/app_constant.dart';
 import 'package:book_app/core/router/app_router.dart';
 import 'package:book_app/core/themes/app_theme.dart';
+import 'package:book_app/features/books/domain/entities/book_entity.dart';
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
 
-void main() {
+void main() async {
   runApp(const MyApp());
+
+  Hive.registerAdapter(BookEntityAdapter());
+
+  await Hive.openBox(AppConstants.kFeaturedBox);
 }
 
 class MyApp extends StatelessWidget {
